e8c2684a9c034b9ec163ef1b8bc29ef34da44727
YogevSaadon <68222705+YogevSaadon@users.noreply.github.com>
2025-08-21 08:09:02 +0300

    html build

--- scripts/game/managers/GameManager.gd
+++ scripts/game/managers/GameManager.gd
@@ -14,7 +14,7 @@ func _ready() -> void:
 func reset_run() -> void:
 	credits = 0
 	coins = 0
-	level_number = 25
+	level_number = 1
 	# Weapon reset now handled by PlayerData.reset()
 
 func next_level() -> void:
